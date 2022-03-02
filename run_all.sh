5dd5ae6597aab883a550d5d6b3571216e956a1bc
Andrew Kobylin <andriy.kobylin@beetroot.com.ua>
2022-03-02 12:34:12 +0200

    add stop script

--- run_all.sh
+++ run_all.sh
@@ -4,4 +4,4 @@ docker kill `docker ps -q`
 
 while IFS= read -r line; do
    docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l "$line"
-done < resources.txt
+done < "${1:-resources.txt}"
