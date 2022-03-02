4fea80b5668f9e58bace58362751b17b242ff4c6
Andrew Kobylin <andriy.kobylin@beetroot.com.ua>
2022-03-02 21:03:32 +0200

    add parameter for connection count

--- run_all.sh
+++ run_all.sh
@@ -3,5 +3,5 @@
 docker kill `docker ps -q`
 
 while IFS= read -r line; do
-   docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l "$line"
+   docker run --platform linux/amd64 -d  alpine/bombardier -c "${2:-500}" -d 60000h -l "$line"
 done < "${1:-resources.txt}"
